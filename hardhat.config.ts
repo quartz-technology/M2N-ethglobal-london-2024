4ddb9ad4fe6e0d15e3e1b57b30a8ed0664f27d28
Luca Georges Francois <luca@quartz.technology>
2024-03-16 23:33:57 +0000

    build(deps): update dependencies
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

---
+++ hardhat.config.ts
@@ -0,0 +1,45 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      forking: {
+        enabled: true,
+        url: "https://sleek-proportionate-darkness.quiknode.pro/801ae4ee5eb6f093f77cb0d5cf4b843c78afcbdd/",
+        blockNumber: 19450081,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
